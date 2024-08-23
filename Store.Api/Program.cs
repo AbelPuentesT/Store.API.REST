5964074e394d9c5dd70ce48164ad8b40a158af44
Abel Alain Puentes Tibaquira <apuentes@mobiletec.net>
2024-08-23 16:22:05 -0500

    Refactor JSON handling and always enable Swagger
    Removed `Newtonsoft.Json.Converters` import and related code from `Program.cs`. Simplified Swagger configuration by enabling it unconditionally. Added `builder.Services.AddSwaggerGenNewtonsoftSupport();` to maintain `Newtonsoft.Json` support in Swagger.

--- Store.Api/Program.cs
+++ Store.Api/Program.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json.Converters;
 using Store.Core.Interfaces;
 using Store.Core.Services;
 using Store.Infrastructure.Data;
@@ -15,11 +14,7 @@ builder.Services.AddDbContext<StoreDbContext>(options =>
 
 ;
 builder.Services.AddControllers();
-//    .AddNewtonsoftJson(o =>
-//{
-//    o.SerializerSettings.Converters.Add(new StringEnumConverter());
-//});
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGenNewtonsoftSupport();
@@ -27,11 +22,9 @@ builder.Services.AddSwaggerGenNewtonsoftSupport();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
